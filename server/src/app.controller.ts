e67a2e323b062da53d401199d8f26358737ff900
Fabien RAKOTOMAMPIANDRA <fabienlintone@yahoo.fr>
2020-07-01 07:21:58 +0300

    Add bcrypt

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+
+import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { User } from './users/users.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req): Promise<User> {
+    return req.user;
   }
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,17 +1,29 @@
 
 import { Injectable } from '@nestjs/common';
-import { UsersService } from '../users/users.service';
+import { UsersService, User } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
 
   async validateUser(username: string, password: string): Promise<any> {
     const user = await this.usersService.findOne(username);
-    if (user && user.password === password) {
-      const { password, ...result } = user;
+    if (user && bcrypt.compare(password, user.password)) {
+      const { pass, ...result } = user;
       return result;
     }
     return null;
+  }
+
+  async login(user: User): Promise<any> {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
